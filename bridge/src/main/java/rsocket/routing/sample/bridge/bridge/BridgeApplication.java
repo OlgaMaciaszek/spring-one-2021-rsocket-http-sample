b7247f4b1c8f2d5ae16176b239c42b098d255408
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-07-12 20:19:03 +0200

    Add Bridge module.

---
+++ bridge/src/main/java/rsocket/routing/sample/bridge/bridge/BridgeApplication.java
@@ -0,0 +1,13 @@
+package rsocket.routing.sample.bridge.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeApplication.class, args);
+	}
+
+}
---
+++ bridge/src/test/java/rsocket/routing/sample/bridge/bridge/BridgeApplicationTests.java
@@ -0,0 +1,14 @@
+package rsocket.routing.sample.bridge.bridge;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
